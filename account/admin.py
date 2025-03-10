bd9f993b44bbf41afd65829e47eebdf487cb3163
AbolfazlAbdi48 <dev.abolfazlabdi@gmail.com>
2025-03-10 16:00:53 +0330

    git init

---
+++ account/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from account.models import User
+
+
+# Register your models here.
+class UserAdminModel(UserAdmin):
+    pass
+
+
+admin.site.register(User, UserAdminModel)
---
+++ extentions/utils.py
@@ -0,0 +1,24 @@
+from . import jalali
+
+
+def jalali_converter_dict(time) -> dict:
+    jmonths = ['فروردین', 'اردیبهشت', 'خرداد', 'تیر', 'مرداد',
+               'شهریور', 'مهر', 'آبان', 'آذر', 'دی', 'بهمن', 'اسفند']
+
+    time_to_str = f'{time.year},{time.month},{time.day}'
+    time_to_tuple = jalali.Gregorian(time_to_str).persian_tuple()
+    time_to_list = list(time_to_tuple)
+
+    for index, month in enumerate(jmonths):
+        if time_to_list[1] == index + 1:
+            time_to_list[1] = month
+            break
+
+    output = f'{time_to_list[2]} {time_to_list[1]} {time_to_list[0]} '
+
+    return {'day': time_to_list[2], 'month': time_to_list[1], 'year': time_to_list[0]}
+
+
+def jalali_converter(time) -> str:
+    str_time = jalali_converter_dict(time)
+    return f"{str_time['day']} {str_time['month']} {str_time['year']}"
