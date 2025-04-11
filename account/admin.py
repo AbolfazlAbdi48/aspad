b13cf8724bb8a2a36988574e6fa972f117f62bcc
AbolfazlAbdi48 <dev.abolfazlabdi@gmail.com>
2025-04-11 21:55:23 +0330

    Add: user profile model

--- account/admin.py
+++ account/admin.py
@@ -1,12 +1,26 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from account.models import User
+from account.models import User, Skill, UserSkillProfile
 
 
 # Register your models here.
+
+
 class UserAdminModel(UserAdmin):
     pass
 
 
 admin.site.register(User, UserAdminModel)
+
+
+@admin.register(UserSkillProfile)
+class UserSkillProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'role')
+    list_filter = ('role',)
+    search_fields = ('user__username', 'user__first_name', 'user__last_name', 'offers__name')
+
+
+@admin.register(Skill)
+class SkillAdmin(admin.ModelAdmin):
+    pass
--- account/models.py
+++ account/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 # Create your models here.
@@ -8,3 +9,42 @@ class User(AbstractUser):
     Customize User's Model.
     """
     pass
+
+
+class Skill(models.Model):
+    name = models.CharField(max_length=255, verbose_name='مهارت')
+
+    class Meta:
+        verbose_name_plural = _('مهارت ها')
+        verbose_name = _('مهارت')
+
+    def __str__(self):
+        return self.name
+
+
+class UserSkillProfile(models.Model):
+    ROLE_CHOICES = [
+        ('coach', 'مربی'),
+        ('vet', 'دامپزشک'),
+        ('gym_owner', 'باشگاه‌دار'),
+        ('shop_owner', 'فروشگاه‌دار'),
+        ('user', 'کاربر عادی'),
+        ('expert', 'کارشناس'),
+    ]
+
+    role = models.CharField(
+        max_length=20,
+        choices=ROLE_CHOICES,
+        default='user',
+        verbose_name=_("نقش کاربر")
+    )
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='skill_profile')
+    offers = models.ManyToManyField(Skill, related_name='offered_by', verbose_name=_('عرضه‌ها'))
+    demands = models.ManyToManyField(Skill, related_name='demanded_by', verbose_name=_('تقاضاها'))
+
+    class Meta:
+        verbose_name_plural = _('پروفایل ها')
+        verbose_name = _('پروفایل')
+
+    def __str__(self):
+        return f"پروفایل مهارتی {self.user.username}"
